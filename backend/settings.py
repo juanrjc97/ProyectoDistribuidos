cd128fdfa2a1c847b89bb7ebad8af4b2fef761c4
juanrjc97 <46859042+juanrjc97@users.noreply.github.com>
2020-08-24 12:05:26 -0500

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 #ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['0.0.0.0', '192.168.100.14','127.0.0.1','172.18.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
